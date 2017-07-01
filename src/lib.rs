5bb018affcf2619cf75fcf09c65e4b8825c31052
Joaquín R <globojorro@gmail.com>
2017-07-01 17:28:09 -0500

    code moved from https://github.com/saibatizoku/benita

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+/// I2C Commands for pH EZO Chip, taken from their Datasheet.
+/// This chip is used for pH measurement. It features calibration, sleep mode,
+/// scale, etc.
+enum PhEzoCommand {
+    Baud(u16),
+    CalibrationClear,
+    CalibrationSingleMidPoint(f64),
+    CalibrationThreePointAtHigh(f64),
+    CalibrationTwoPointAtLow(f64),
+    DeviceAddress(u8),
+    DeviceInformation,
+    Export,
+    ExportInfo,
+    Factory,
+    Find,
+    Import(String),
+    LedOn,
+    LedOff,
+    LedState,
+    ProtocolLockDisable,
+    ProtocolLockEnable,
+    ProtocolLockStatus,
+    Reading,
+    Sleep,
+    Slope,
+    Status,
+    TemperatureCompensation,
+    TemperatureCompensatedValue,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
