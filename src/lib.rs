4a8a8a9625a46b147f34cf34d1572114e7bcbe8d
Joaquín R <globojorro@gmail.com>
2017-07-02 02:01:45 -0500

    annotate PhCommands

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,44 @@
-/// I2C Commands for pH EZO Chip, taken from their Datasheet.
-/// This chip is used for pH measurement. It features calibration, sleep mode,
-/// scale, etc.
-enum PhEzoCommand {
+//! I2C Commands for pH EZO Chip, taken from their Datasheet.
+//! This chip is used for pH measurement. It features calibration, sleep mode,
+//! scale, etc.
+
+pub enum PhCommand {
+    // 'Baud' command
     Baud(u16),
+    // 'Cal' command
     CalibrationClear,
     CalibrationSingleMidPoint(f64),
     CalibrationThreePointAtHigh(f64),
     CalibrationTwoPointAtLow(f64),
-    DeviceAddress(u8),
-    DeviceInformation,
+    // 'Export/Import' command
     Export,
     ExportInfo,
-    Factory,
-    Find,
     Import(String),
+    // 'Factory' command
+    Factory,
+    // 'Find' command
+    Find,
+    // 'I' command
+    DeviceInformation,
+    // 'I2C' command
+    DeviceAddress(u8),
+    // 'L' command
     LedOn,
     LedOff,
     LedState,
+    // 'Plock' command
     ProtocolLockDisable,
     ProtocolLockEnable,
     ProtocolLockStatus,
+    // 'R' command
     Reading,
+    // 'Sleep' command
     Sleep,
+    // 'Slope' command
     Slope,
+    // 'Status' command
     Status,
+    // 'T' command
     TemperatureCompensation,
     TemperatureCompensatedValue,
 }
