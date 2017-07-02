8b79e0e9ec3ac9bce5afc1aeb6394b81680b6e4e
Joaquín R <globojorro@gmail.com>
2017-07-02 02:03:30 -0500

    boilerplate tests for 'Baud'

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,70 @@ pub enum PhCommand {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use super::PhCommand::*;
+
     #[test]
-    fn it_works() {
+    fn build_command_baud_300() {
+        let cmd = Baud(BpsRate::Bps300).build();
+        assert_eq!(cmd.command, "Baud,300\0");
+        assert_eq!(cmd.delay, None);
+        assert_eq!(cmd.response, None);
+    }
+
+    #[test]
+    fn build_command_baud_1200() {
+        let cmd = Baud(BpsRate::Bps1200).build();
+        assert_eq!(cmd.command, "Baud,1200\0");
+        assert_eq!(cmd.delay, None);
+        assert_eq!(cmd.response, None);
+    }
+
+    #[test]
+    fn build_command_baud_2400() {
+        let cmd = Baud(BpsRate::Bps2400).build();
+        assert_eq!(cmd.command, "Baud,2400\0");
+        assert_eq!(cmd.delay, None);
+        assert_eq!(cmd.response, None);
+    }
+
+    #[test]
+    fn build_command_baud_9600() {
+        let cmd = Baud(BpsRate::Bps9600).build();
+        assert_eq!(cmd.command, "Baud,9600\0");
+        assert_eq!(cmd.delay, None);
+        assert_eq!(cmd.response, None);
+    }
+
+    #[test]
+    fn build_command_baud_19200() {
+        let cmd = Baud(BpsRate::Bps19200).build();
+        assert_eq!(cmd.command, "Baud,19200\0");
+        assert_eq!(cmd.delay, None);
+        assert_eq!(cmd.response, None);
+    }
+
+    #[test]
+    fn build_command_baud_38400() {
+        let cmd = Baud(BpsRate::Bps38400).build();
+        assert_eq!(cmd.command, "Baud,38400\0");
+        assert_eq!(cmd.delay, None);
+        assert_eq!(cmd.response, None);
+    }
+
+    #[test]
+    fn build_command_baud_57600() {
+        let cmd = Baud(BpsRate::Bps57600).build();
+        assert_eq!(cmd.command, "Baud,57600\0");
+        assert_eq!(cmd.delay, None);
+        assert_eq!(cmd.response, None);
+    }
+
+    #[test]
+    fn build_command_baud_115200() {
+        let cmd = Baud(BpsRate::Bps115200).build();
+        assert_eq!(cmd.command, "Baud,115200\0");
+        assert_eq!(cmd.delay, None);
+        assert_eq!(cmd.response, None);
     }
 }
